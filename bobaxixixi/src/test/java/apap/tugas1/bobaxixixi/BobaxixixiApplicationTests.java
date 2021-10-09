678d5299d97924b489778ba054ef3c949b84af9f
Della Patricia Siregar <dellapatricia@Dellas-MacBook-Pro.local>
2021-10-09 16:09:56 +0700

    feat: tugas 1 - initialize spring boot project

---
+++ bobaxixixi/src/test/java/apap/tugas1/bobaxixixi/BobaxixixiApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tugas1.bobaxixixi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobaxixixiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
